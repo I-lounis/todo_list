eaf1210c3596b6bd34715e7e970bd76646245803
lamia 8512 <elf.lamia@hotmail.fr>
2025-10-07 12:16:51 +0200

    création base de données

---
+++ init/database.sql
@@ -0,0 +1,15 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS `listdb`;
+USE `listdb`;
+
+CREATE TABLE IF NOT EXISTS `todos`(
+    `id_todos` Int Auto_increment NOT NULL,
+    `title` Varchar (150) NOT NULL,
+    `description` Text,
+    `status` Varchar (255) NOT NULL,
+    CONSTRAINT `todos_pk` PRIMARY KEY (`id_todos`)
+);
+
+
